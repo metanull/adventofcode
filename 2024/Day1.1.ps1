3a261196f0f28f587eb5baa6885df6e7db5f7ad5
Pascal Havelange <havelangep@hotmail.com>
2024-12-04 14:56:57 +0100

    2024 Day 1

---
+++ 2024/Day1.1.ps1
@@ -0,0 +1,15 @@
+﻿# https://adventofcode.com/2024/day/1
+$Data = Get-Content (Join-Path $PSScriptRoot Input/Day1.txt -Resolve) | Foreach-Object {
+    $s=$_ -split '\s+'
+    [pscustomobject]@{Left=$s[0];Right=$s[1]}
+}
+$Right = ($Data.Right | Sort-Object)
+$Left = ($Data.Left | Sort-Object)
+
+$k = 0
+$Left | ForEach-Object {
+    $L = $_
+    $R = $Right | Select-Object -First 1 -Skip $k
+    [Math]::Abs($L -$R) | Write-Output
+    $k++
+} | Measure-Object -Sum
---
+++ 2024/Day1.2.ps1
@@ -0,0 +1,13 @@
+﻿# https://adventofcode.com/2024/day/1#part2
+$Data = Get-Content (Join-Path $PSScriptRoot Input/Day1.txt -Resolve) | Foreach-Object {
+    $s=$_ -split '\s+'
+    [pscustomobject]@{Left=$s[0];Right=$s[1]}
+}
+$Right = ($Data.Right | Sort-Object)
+$Left = ($Data.Left | Sort-Object)
+
+$Left | ForEach-Object {
+    $L = [int]$_
+    $R = ($Right | Where-Object { $_ -eq $L }).Count
+    ($L * $R)
+} | Measure-Object -Sum
