8f3d0c60a53fe1c57fbfedd175f62a64f0df271c
Pascal HAVELANGE <havelangep@hotmail.com>
2024-12-04 22:59:42 +0100

    2023/1.1

--- 2023/Day1.1.ps1
+++ 2023/Day1.1.ps1
@@ -1,9 +1,26 @@
 Process {
+    $RegEx = [Regex]::new('^\D*(\d).*(\d)\D*$')
     $InputData | ForEach-Object {
         $Line = $_
-    }
+        if($Line -match '^\D*(\d).*(\d)\D*$') {
+            $V = "$([int]$matches[1])$([int]$matches[2])"
+            $V | Write-Output
+        } elseif($Line -match '^\D*(\d)\D*$') {
+            $V = "$([int]$matches[1])$([int]$matches[1])"
+            $V | Write-Output
+        } else {
+            $Line | Write-Warning
+        }
+    } | Measure-Object -Sum
 }
 Begin {
     $InputPath = Join-Path -Resolve -Path $PSScriptRoot -ChildPath "Input\$(($PSCommandPath | Split-Path -Leaf) -replace '\.\d+\.ps1','.txt')" -ErrorAction Stop
     $InputData = Get-Content $InputPath -ErrorAction Stop
+<#
+    $InputData = @('1abc2'
+    'pqr3stu8vwx'
+    'a1b2c3d4e5f'
+    'treb7uchet'
+    )
+#>
 }
