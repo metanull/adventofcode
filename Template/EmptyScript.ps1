a8abdc6f374fe03a285cba5026ccfb356a0d5fbf
Pascal Havelange <pascal.havelange@eesc.europa.eu>
2024-12-05 10:49:10 +0100

    Update to the template script (opens browser to download input file)

--- Template/EmptyScript.ps1
+++ Template/EmptyScript.ps1
@@ -4,6 +4,13 @@ Process {
     }
 }
 Begin {
-    $InputPath = Join-Path -Resolve -Path $PSScriptRoot -ChildPath "Input\$(($PSCommandPath | Split-Path -Leaf) -replace '\.\d+\.ps1','.txt')" -ErrorAction Stop
+    $PuzzleUrl = "https://adventofcode.com/$(Get-Date -Format 'yyyy')/day/$([int](Get-Date -Format 'dd'))"
+    $InputPath = Join-Path -Path $PSScriptRoot -ChildPath "Input\$(($PSCommandPath | Split-Path -Leaf) -replace '\.\d+\.ps1','.txt')"
+    if(-Not (Test-Path $InputPath)) {
+        $InputUrl = "$PuzzleUrl/input"
+        Start-Process $InputUrl
+        throw "Input file is missing. Please download the Input file at $InputUrl, and save it as $InputPath"
+    }
+    "Instructions: $PuzzleUrl" | Write-Warning
     $InputData = Get-Content $InputPath -ErrorAction Stop
 }
