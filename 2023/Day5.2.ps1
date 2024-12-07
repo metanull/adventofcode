4a0bf085720ab16e6220549a880e9cfddd50ccce
Pascal HAVELANGE <havelangep@hotmail.com>
2024-12-07 13:49:57 +0100

    new idea - WIP

--- 2023/Day5.2.ps1
+++ 2023/Day5.2.ps1
@@ -4,39 +4,106 @@ param ()
 Process {
     $Order = @('seed','soil','fertilizer','water','light','temperature','humidity','location')
     
-        $Index = 0
+    # Get two maps, filling blanks
+    $Max = [Math]::pow(2,32) # ([int32]::MaxValue+1)*2 # 4294967296 # [int64]::MaxValue
+    $PairOfMaps = [pscustomobject]@{Left=@{};Right=@()}
+    $Index = 0
+    for($n=0; $n -lt 2; $n++) {
+        $Index = $Index + $n
         $TempMap = Get-MappedRanges -Maps $Maps -SourceName ($Order[$Index]) -DestinationName ($Order[$Index + 1]) | Sort-Object start
-        # Fill the gaps
-        $prev = 0
-        $MapLeft = for($i=0;$i -lt $TempMap.Count;$i++) {
-            if($TempMap[$i].start -ne $prev) {
-                [pscustomobject]@{start = $prev;end = $TempMap[$i].start;offset = 0;offstart = $prev;offend = $TempMap[$i].start}
-            } else {
-                $TempMap[$i]
-            }
-            if($i -eq ($TempMap.Count -1) -and $TempMap[$i].end -lt 1000000000000000) {
-                [pscustomobject]@{start = $TempMap[$i].end;end = 1000000000000000;offset = 0;offstart = $TempMap[$i].end;offend = 1000000000000000}
-            }
-            $prev = $TempMap[$i].end
+        if($n -eq 0) {
+            $PairOfMaps.Left = Fill-Gaps -TempMap $TempMap -Index $Index -Max $Max
+        } else {
+            $PairOfMaps.Right = Fill-Gaps -TempMap $TempMap -Index $Index -Max $Max
         }
+    }
+
+    # Merge the two maps
+    for($l = 0; $l -lt $PairOfMaps.Left.Count; $l ++) {
+        # Look for the start of the right map
+        $RightStartIndex = $null
+        for($r = 0; $r -lt $PairOfMaps.Right.Count; $r ++) {
+            if(     $PairOfMaps.Left[$l].offstart -ge $PairOfMaps.Right[$r].start `
+                -and $PairOfMaps.Left[$l].offstart -le $PairOfMaps.Right[$r].end 
+            ) {
+                $RightStartIndex = $r
+                break;
+            }
+        }
+        if($null -eq $RightStartIndex) {
+            throw "No right map found for $($PairOfMaps.Left[$l].offstart)"
+        }
+        "Right map found for $($PairOfMaps.Left[$l].offstart) at $RightStartIndex" | Write-Warning
+
+        "Right map can provide $($PairOfMaps.Right[$RightStartIndex].end - $PairOfMaps.Right[$RightStartIndex].start) items" | Write-Warning
+        "Left map requires $($PairOfMaps.Left[$l].end - $PairOfMaps.Left[$l].start) items" | Write-Warning
         
+        # Take what we have
+        $Slice = $PairOfMaps.Left[$l].PSObject.Copy()
+        $Slice.start = $PairOfMaps.Left[$l].start
+        $Slice.end = $Slice.start + [Math]::Min(
+            ($PairOfMaps.Right[$RightStartIndex].end - $PairOfMaps.Right[$RightStartIndex].start),
+            ($PairOfMaps.Left[$l].end - $PairOfMaps.Left[$l].start)
+        )
 
-        $Index = 1
-        $TempMap = Get-MappedRanges -Maps $Maps -SourceName ($Order[$Index]) -DestinationName ($Order[$Index + 1]) | Sort-Object start
-        # Fill the gaps
-        $prev = 0
-        $MapRight = for($i=0;$i -lt $TempMap.Count;$i++) {
-            if($TempMap[$i].start -ne $prev) {
-                [pscustomobject]@{start = $prev;end = $TempMap[$i].start;offset = 0;offstart = $prev;offend = $TempMap[$i].start}
-            } else {
-                $TempMap[$i]
-            }
-            if($i -eq ($TempMap.Count -1) -and $TempMap[$i].end -lt 1000000000000000) {
-                [pscustomobject]@{start = $TempMap[$i].end;end = 1000000000000000;offset = 0;offstart = $TempMap[$i].end;offend = 1000000000000000}
-            }
-            $prev = $TempMap[$i].end
+        $Slice.offset = $PairOfMaps.Right[$RightStartIndex].offset
+        $Slice.offstart = $Slice.start + $Slice.Offset
+        $Slice.offend = $Slice.end + $Slice.Offset
+        $Slice | Write-Output
+
+        # Print what we miss
+        "We miss $($($PairOfMaps.Left[$l].end - $PairOfMaps.Left[$l].start) - $Slice.end)" | Write-Warning
+
+        if( ($PairOfMaps.Right[$RightStartIndex].end - $PairOfMaps.Right[$RightStartIndex].start) - ($PairOfMaps.Left[$l].end - $PairOfMaps.Left[$l].start) -ge 0) {
+            "Right map can provide enough items" | Write-Warning
+            # nothing else to do here, continue with left map
+        } else {
+            "Right map can't provide enough items" | Write-Warning
+            # Take what we have...
+            # = repeat mor eor less what we did above
+            # ...Then Look for more
+            # to do so, it would be interrest to save the current item of the left map in a temp variable
+            # so that it can be updated before continuing the loop
         }
+    }
+<#
+    # Merge the two maps
+    for($l = 0; $l -lt $PairOfMaps.Left.Count; $l ++) {
+        for($r = 0; $r -lt $PairOfMaps.Right.Count; $r ++) {
+            if($PairOfMaps.Left[$l].end -le $PairOfMaps.Right[$l].start ) {
+                # Direct translation, use the offset from right, with start and end from left
+                $Merged += @{
+                    start = $PairOfMaps.Left[$l].start
+                    end = $PairOfMaps.Left[$l].end
+                    offset = $PairOfMaps.Right[$r].offset
+                    offstart = $PairOfMaps.Left[$r].offstart + $PairOfMaps.Right[$r].offset
+                    offend = $PairOfMaps.Left[$r].offend + $PairOfMaps.Right[$r].offset
+                }
+            } elseif($PairOfMaps.Left[$l].end -le $PairOfMaps.Right[$l].end) {
+                # Section, use a slice of left with a slice of right
+                $Merged += @{
+                    start = $PairOfMaps.Left[$l].start
+                    end = $PairOfMaps.Left[$l].end
+                    offset = $PairOfMaps.Right[$r].offset
+                    offstart = $PairOfMaps.Left[$r].offstart + $PairOfMaps.Right[$r].offset
+                    offend = $PairOfMaps.Left[$r].offend + $PairOfMaps.Right[$r].offset
+                }
+            }
 
+            if($PairOfMaps.Left[$l].start -le $PairOfMaps.Right[$r].start -and $PairOfMaps.Left[$l].end -ge $PairOfMaps.Right[$r].end) {
+                $PairOfMaps.Left[$l].offset = $PairOfMaps.Right[$r].start - $PairOfMaps.Left[$l].start
+                $PairOfMaps.Left[$l].offstart = $PairOfMaps.Right[$r].start
+                $PairOfMaps.Left[$l].offend = $PairOfMaps.Right[$r].end
+            }
+            if($PairOfMaps.Right[$r].start -le $PairOfMaps.Left[$l].start -and $PairOfMaps.Right[$r].end -ge $PairOfMaps.Left[$l].end) {
+                $PairOfMaps.Right[$r].offset = $PairOfMaps.Left[$l].start - $PairOfMaps.Right[$r].start
+                $PairOfMaps.Right[$r].offstart = $PairOfMaps.Left[$l].start
+                $PairOfMaps.Right[$r].offend = $PairOfMaps.Left[$l].end
+            }
+        }
+    }
+#>
+<#
         $BiMap = for($n = 0; $n -lt [Math]::Max($MapLeft.Count, $MapRight.Count); $n++) {
             $obj = [pscustomobject]@{}
             if($n -lt $MapLeft.Count) {
@@ -56,7 +123,7 @@ Process {
             $obj | Write-Output
         }
         $BiMap | Write-Output
-    
+    #>
     <#
     $Lowest = $null
     $n = $Seeds.Length
@@ -112,6 +179,38 @@ Begin {
             }
         } else {
             throw "Unexpected line: $Line"
+        }
+    }
+
+    Function Fill-Gaps {
+        param(
+            [Alias('Map')]
+            [object[]]$TempMap,
+            [long]$Index,
+            [long]$Max = [Math]::pow(2,32)
+        )
+        $prev = 0
+        for($i=0;$i -lt $TempMap.Count;$i++) {
+            # Write-Warning "$i Prev:$Prev; Start:$($TempMap[$i].start); End:$($TempMap[$i].end)"
+            if($TempMap[$i].start -ne $prev) {
+                # Write-Warning "$i Filling GAP:  Start:$($prev); End:$($TempMap[$i].start); Offset:0)"
+                [pscustomobject]@{start = $prev;end = $TempMap[$i].start;offset = 0;offstart = $prev;offend = $TempMap[$i].start} | Write-Output 
+                $prev = $TempMap[$i].start
+                if($Prev -ne 0) {
+                    # Filling the gap didn't reach the current's start, so we need to repeat the current
+                    # Write-Warning "REPEAT"
+                    $i --
+                }
+            } else {
+                # Write-Warning "$i Adding AS IS: Start:$($TempMap[$i].start); End:$($TempMap[$i].end); Offset:$($TempMap[$i].offset)"
+                $TempMap[$i] | Write-Output
+                $prev = $TempMap[$i].end
+            }
+            if($i -eq ($TempMap.Count -1) -and $TempMap[$i].end -lt $Max) {
+                # Write-Warning "$i Adding LAST:  Start:$($TempMap[$i].end); End:$($Max); Offset:0)"
+                [pscustomobject]@{start = $TempMap[$i].end;end = $Max;offset = 0;offstart = $TempMap[$i].end;offend = $Max} | Write-Output
+                $prev = $TempMap[$i].start
+            }
         }
     }
 
