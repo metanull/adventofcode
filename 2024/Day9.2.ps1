dbd05f3f783595dcc7d45d5b639de0b25bf33a6c
Pascal Havelange <pascal.havelange@eesc.europa.eu>
2024-12-12 17:06:41 +0100

    2024 Day9.2 WIP

--- 2024/Day9.2.ps1
+++ 2024/Day9.2.ps1
@@ -1,122 +1,136 @@
 Process {
-    $DiskImage = ($InputData | ExpandImage | %{if($null -eq $_){'.'}else{$_}}) -join ''
+    $DiskImage = $InputData
 
-    $d = $DiskImage | DefragOneBlock
-    $b = BlockToImage -BlockList $d
-    $p = $b
-    while($true){
-        $d = $b | DefragOneBlock
-        $b = BlockToImage -BlockList $d
-        $b
-        if($b -eq $p) {
-            break
+    $DiskImage | Write-Warning
+
+    ExpandImage -InputData $DiskImage | Write-Warning
+    '00992111777.44.333....5555.6666.....8888..'|write-warning
+
+    $DiskImageInt = ([char[]]$DiskImage)|%{CtoI($_) }
+    $ClonedImage = $DiskImageInt.PSObject.Copy()
+    $DiskImageInt -join ','
+    $Moved = ([pscustomobject]@{List=@()})
+    Defrag -Input ([ref]$DiskImageInt) -Moved ([ref]$Moved)
+    $DiskImageInt -join ','
+
+    $Index = 0
+    $Position = 0
+    ($DiskImageInt |% {
+        # pour les paies (car 1 sur deux est un data)
+        for($i=0; $i -lt $_; $i ++) {
+            $Position | Write-Output
+            $Index ++
         }
-        $p = $b.PSObject.Copy()
+        $Moved | Where-Object { $_.to -eq $Position } {
+            for($i=0; $i -lt $_; $i ++) {
+                $_.from | Write-Output
+                $Index ++
+            }
+        }
+        #Pour l'élément suivant (car 1 sur deux est un free space)
+        for($i=0; $i -lt $_; $i ++) {
+            $Position | Write-Output
+            $Index ++
+        }
+
+
+        $Position ++
+    }) -join ''
+
+    $Index = 0
+    $Position = 0
+    $DiskImageInt |% {
+        for($i=0; $i -lt $_; $i ++) {
+            $Position * $Index | Write-Output
+            $Index ++
+        }
+        $Moved | Where-Object { $_.to -eq $Position } {
+            for($i=0; $i -lt $_; $i ++) {
+                $_.from * $Index | Write-Output
+                $Index ++
+            }
+        }
+        $Position ++
     }
+<#    $p = 0
+    for($x = 0; $x -lt $ClonedImage.Count; $x++) {
+        $Defrag = $DiskImageInt[$x]
+        $Original = $ClonedImage[$x]
+
+        if($x %2 -eq 0) {
+            # Data
+            for($n = 0; $n -lt $Defrag; $n++) {
+                $Defrag * ($p++)    # Checksum      pas just
+            }
+        } else {
+            # Free space
+            $Moved | Where-Object {
+                $_.to -eq $p
+            } | ForEach-Object {
+                $oldIndex = $_.from
+                # Count blocks before the "from", this was the "file index"
+                $oldFileIndex = $ClonedImage|Where-Object{$_ -lt $oldIndex}|Measure-Object -Sum|Select-Object -ExpandProperty Sum
+                $oldLength = $ClonedImage[$oldIndex]
+                for($n = 0; $n -lt $oldLength; $n++) {
+                    $Defrag * ($p++)    # Checksum      pas juste
+                }
+            }
+            $p ++
+        }
+    }
+    #>
 }
 Begin {
-    Function BlockToImage {
-        [OutputType([string])]
+    Function Defrag {
         [CmdletBinding()]
-        param(
-            [Parameter(Mandatory)]
-            [object[]]$BlockList
-        )
-        ($BlockList |% {$_.Value}) -join ''
-    }
-    Function DefragOneBlock {
         [OutputType([object[]])]
-        [CmdletBinding()]
-        param(
-            [Parameter(Mandatory,ValueFromPipeline)]
-            [String]$DiskImage
-        )
+        param([ref]$InputData,[ref]$Moved)
 
-        $BlockExpr = [regex]::new('(?<block>(\d|\.)(\1*))')
-        $BlockList = $BlockExpr.Matches($DiskImage) | Select-Object Success,Index,Value,Length
-        for($z = $BlockList.count; $z -gt 0; $z --) {
-            if($BlockList[$z].Value -notmatch '^\d+$') {
-                # Not a File block, skip
-                continue
-            }
-            for($a = 0; $a -lt $BlockList.count; $a ++) {
-                if($BlockList[$a].Value -notmatch '^\.+$') {
-                    # Not a Free block, skip
-                    continue
+        $MovedNodes = @()
+        $LastVal = ([Math]::Truncate(($InputData.Value.Count)/2)) * 2
+        # Browse all value blocks form the end
+        for($z=$LastVal; $z -ge 0; $z -= 2) {
+            $DataLength = $InputData.Value[$z]
+            # Browse all free blocks form the start
+            for($a=1; $a -lt $InputData.Value.Count; $a += 2) {
+                $FreeLength = [Math]::Abs($InputData.Value[$a])
+                if($FreeLength -ge $DataLength) {
+                    # Found free space, move block
+                    $MovedNodes += [pscustomobject]@{from=$z;to=$a}
+                    $InputData.Value[$z] = $FreeLength - $DataLength
+                    $InputData.Value[$a] = -($FreeLength - $DataLength)
+                    break
                 }
-                if($BlockList[$a].Length -lt $BlockList[$z].Length) {
-                    # Free block is not large enough
-                    continue
-                }
-                # Move the block
-                $RoomLeft = 0
-                for($m = 0; $m -lt $BlockList.count; $m ++) {
-                    if($m -eq $a) {
-                        # Handle the ex-empty block
-                        $RoomLeft = $BlockList[$a].Length - $BlockList[$z].Length
-                        $Block = $BlockList[$a] | Select-Object Success,Index,Value,Length
-                        $Block.Length = $BlockList[$z].Length
-                        $Block.Value = ''.PadLeft($BlockList[$z].Length,($BlockList[$z].Value[0]))
-                        $Block | Write-Output
-                        if($RoomLeft -gt 0) {
-                            $Block = $BlockList[$a] | Select-Object Success,Index,Value,Length
-                            $Block.Length = $RoomLeft
-                            $Block.Value = ''.PadLeft($RoomLeft,'.')
-                            $Block | Write-Output
-                        }
-                    } elseif($m -eq $z) {
-                        # Handle the ex-file block
-                        $Block = $BlockList[$z] | Select-Object Success,Index,Value,Length
-                        $Block.Value = ''.PadLeft($Block.Length,'.')
-                        $Block | Write-Output
-                        return
-                    } else {
-                        $Block = $BlockList[$m] | Select-Object Success,Index,Value,Length
-                        $Block | Write-Output
-                    }
-                }
-                return
             }
         }
+        $Moved.Value = $MovedNodes
+        #return $InputData
+    }
 
+    Function CtoI {
+        param([parameter(Position=0,Mandatory)][ValidateRange('0','9')][char]$c)
+        return ([int]$c) - ([int]([char]'0'))
+    }
+    Function ItoC {
+        param([parameter(Position=0,Mandatory)][ValidateRange(0,9)][int]$i)
+        return (""+$i)[0]
     }
     Function ExpandImage {
-        [OutputType([int[]])]
         [CmdletBinding()]
-        param(
-            [OutputType([int[]])]
-            [Parameter(Mandatory,ValueFromPipeline)]
-            [String]$DiskImage
-        )
-        Begin {
-            $Parser = [regex]::new('(?<digit>\d)')
-        }
-        Process {
-            $CursorIndex = 0
-            $FileIndex = 0
-            $DiskImage | ForEach-Object {
-                $Parser.Matches($_) | Where-Object {
-                    $_.Success
-                } | ForEach-Object {
-                    [int]($_.Value) | Write-Output
+        [OutputType([string])]
+        param([char[]]$InputData)
+        $s = [string]::empty
+        for($i=0;$i -lt $InputData.Count; $i++) {
+            for($j=0;$j -lt (CtoI($InputData[$i])); $j ++) {
+                if($i % 2 -eq 0) {
+                    $s += "$([Math]::Truncate($i/2))"
+                } else {
+                    $s += '.'
                 }
-            } | Foreach-Object {
-                $BlockSize = $_
-                for($n = 0; $n -lt $BlockSize; $n ++ ) {
-                    if($CursorIndex % 2 -eq 0) {
-                        $FileIndex | Write-Output
-                    } else {
-                        $null | Write-Output
-                    }
-                }
-                if($CursorIndex % 2 -ne 0) {
-                    $FileIndex ++
-                }
-                $CursorIndex ++
             }
         }
+        return $s
     }
-
     $Year = 2024
     $Day = 9
     $PuzzleUrl = "https://adventofcode.com/$Year/$Day"
