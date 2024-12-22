075100ad4b0fd5bd62436dec1dca875908b82e22
Pascal Havelange <havelangep@hotmail.com>
2024-12-22 16:55:20 +0100

    2024 Day13.1 Done

--- 2024/Day13/Day13.1.ps1
+++ 2024/Day13/Day13.1.ps1
@@ -1,77 +1,44 @@
 # https://adventofcode.com/2024/day/13
 Process {
-    <#
     $InputData | ForEach-Object {
-        $_ 
-    } | ft
-     #>
-    $InputData | ForEach-Object {
-
         # Write-Warning "Prize($($_.Prize.X), $($_.Prize.Y)); A($($_.A.X), $($_.A.Y)); B($($_.B.X), $($_.B.Y))"
-        
         $Solutions = @()
         for($i = 0; $i -lt 100; $i ++) {
-
-            $MulA = $_.A.PSObject.Copy()
-            $MulA.X *= $i
-            $MulA.Y *= $i
-
-            $MulB = $_.B.PSObject.Copy()
-            $MulB.X *= $i
-            $MulB.Y *= $i
-
-            $SolA = $_.Prize.PSObject.Copy()
-            $SolA.X -= $MulA.X
-            $SolA.Y -= $MulA.Y
-
-            $SolB = $_.Prize.PSObject.Copy()
-            $SolB.X -= $MulB.X
-            $SolB.Y -= $MulB.Y
+            $MulA = $_.A.PSObject.Copy();       $MulA.X *= $i; $MulA.Y *= $i
+            $MulB = $_.B.PSObject.Copy();       $MulB.X *= $i; $MulB.Y *= $i
+            $SolA = $_.Prize.PSObject.Copy();   $SolA.X -= $MulA.X; $SolA.Y -= $MulA.Y
+            $SolB = $_.Prize.PSObject.Copy();   $SolB.X -= $MulB.X; $SolB.Y -= $MulB.Y
 
             # Write-Warning "A*i($($MulA.X), $($MulA.Y)); B*i($($MulB.X), $($MulB.Y)); Prize-(A*i)($($SolA.X), $($SolA.Y)); Prize-(B*i)($($SolB.X), $($SolB.Y))"
-
             if(($SolA.X -lt 0 -or $SolA.Y -lt 0) -and ($SolB.X -lt 0 -or $SolB.Y -lt 0)) {
-                # Write-Warning "No solution found"
+                #Write-Warning "No solution found"
                 break
             }
-
             if($SolA.X -eq 0 -and $SolA.Y -eq 0) {
-                # Write-Warning "Solution found: A=$i, B=0"
-                $Solutions += ,([pscustomobject]@{
-                    A = $i
-                    B = 0
-                })
+                #Write-Warning "Solution found: A=$i, B=0"
+                $Solutions += ,([pscustomobject]@{ A = $i; B = 0})
             }
             if($SolB.X -eq 0 -and $SolB.Y -eq 0) {
-                # Write-Warning "Solution found: A=0, B=$i"
-                $Solutions += ,([pscustomobject]@{
-                    A = 0
-                    B = $i
-                })
+                #Write-Warning "Solution found: A=0, B=$i"
+                $Solutions += ,([pscustomobject]@{ A = 0; B = $i})
             }
             if($SolA.X % $_.B.X -eq 0) {
                 $N = $SolA.X / $_.B.X
-                if($SolA.Y / $_.B.Y -eq $N) {
-                    # Write-Warning "Solution found: A=$i, B=$N"
-                    $Solutions += ,([pscustomobject]@{
-                        A = $i
-                        B = $N
-                    })
+                if($N -gt 0 -and $SolA.Y / $_.B.Y -eq $N) {
+                    #Write-Warning "Solution found (A): A=$i, B=$N"
+                    $Solutions += ,([pscustomobject]@{ A = $i; B = $N})
                 }
             }
             if($SolB.X % $_.A.X -eq 0) {
                 $N = $SolB.X / $_.A.X
-                if($SolB.Y / $_.A.Y -eq $N) {
-                    # Write-Warning "Solution found: A=$N, B=$i"
-                    $Solutions += ,([pscustomobject]@{
-                        A = $N
-                        B = $i
-                    })
+                if($N -gt 0 -and $SolB.Y / $_.A.Y -eq $N) {
+                    #Write-Warning "Solution found (B): A=$N, B=$i"
+                    $Solutions += ,([pscustomobject]@{ A = $N; B = $i})
                 }
             }
         }
         if($Solutions) {
-            # Write-Warning "Solutions found: $(($Solutions|Select-Object -Unique A,B).Count)"
+            # Write-Warning "Solution(s) found: $(($Solutions|Select-Object -Unique A,B).Count)"
             $_ | Add-Member -MemberType NoteProperty -Name Solutions -Value ($Solutions|Select-Object -Unique A,B) -PassThru
         }
     } | Where-Object {$_.Solutions.Count -gt 0} | Foreach-Object {
@@ -82,8 +49,10 @@ Process {
             } -Process {
                 $Cost = ($_.A * $CostA) + ($_.B * $CostB)
                 $_ | Add-Member -MemberType NoteProperty -Name Cost -Value $Cost -PassThru
-            } | Sort-Object -Property Cost -Descending | Select-Object -First 1
-        $BestSolution.Cost | Write-Output
+            } | Sort-Object -Property Cost | Select-Object -First 1
+        $Machine | Add-Member -MemberType NoteProperty -Name BestSolution -Value $BestSolution
+
+        $Machine.BestSolution.Cost | Write-Output
     } | Measure-Object -Sum | Select-Object -ExpandProperty Sum
 }
 Begin {
@@ -137,8 +106,6 @@ Begin {
         $Parser = [regex]::new('^(?<Label>.*?): X[=\+](?<X>\d+), Y[=\+](?<Y>\d+)$')
         $Buffer = @()
         $Object = $null
-    } -End {
-        BufferToObject -Buffer ($Buffer) | Write-Output
     } -Process {
         if(-not $_) {
             $Buffer = @()
