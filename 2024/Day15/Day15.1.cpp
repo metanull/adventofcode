98826c5be8bca95fc5d0709b8e7b14176b2e26ff
Pascal HAVELANGE <havelangep@hotmail.com>
2025-01-07 23:41:50 +0100

    2024 Day 15.1

--- 2024/Day15/Day15.1.cpp
+++ 2024/Day15/Day15.1.cpp
@@ -8,6 +8,7 @@
 #include <map>
 #include <map>
 #include <stdexcept>
+#include <set>
 // #include <compare>
 
 #include "..\AdventOfCode.h"
@@ -67,16 +68,51 @@ std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<char>> 
 }
 
 void MoveRobot(std::vector<std::vector<char>> & map, std::pair<int,int> & Robot, std::pair<int,int> & direction) {
+
+    // Print the move meta data
+    // std::cout << "BEFORE: Robot:" << Robot << std::endl;
+    // std::cout << "BEFORE: Velocity:" << direction << std::endl;
+
+    // Collect what is ahead of the robot (until a wall, the end of the map, or the maximum required number of moves is reached)
     std::vector<char> ahead;
+    int crateCount = 0;
     if(direction.first == 0) {
         // Move is vertical
-        for(auto p = Robot.second + (direction.second > 0 ? 1 : -1); p >= 0 && p < map.size(); p += (direction.second > 0 ? 1 : -1)) {
-            ahead.push_back(map[p][Robot.first]);
+        if(direction.second > 0) {
+            for(auto p = Robot.second + 1; p <= Robot.second + direction.second + crateCount && p < map.size(); p++) {
+                crateCount += (map[p][Robot.first] == CRATE_CHAR);
+                ahead.push_back(map[p][Robot.first]);
+                if(map[p][Robot.first] == WALL_CHAR) {
+                    break;
+                }
+            }
+        } else {
+            for(auto p = Robot.second - 1; p >= Robot.second + direction.second - crateCount && p >= 0; p--) {
+                crateCount += (map[p][Robot.first] == CRATE_CHAR);
+                ahead.push_back(map[p][Robot.first]);
+                if(map[p][Robot.first] == WALL_CHAR) {
+                    break;
+                }
+            }
         }
     } else {
         // Move is horizontal
-        for(auto p = Robot.first + (direction.first > 0 ? 1 : -1); p >= 0 && p < map[Robot.second].size(); p += (direction.first > 0 ? 1 : -1)) {
-            ahead.push_back(map[Robot.second][p]);
+        if(direction.first > 0) {
+            for(auto p = Robot.first + 1; p <= Robot.first + direction.first + crateCount && p < map[Robot.second].size(); p++) {
+                crateCount += (map[Robot.second][p] == CRATE_CHAR);
+                ahead.push_back(map[Robot.second][p]);
+                if(map[Robot.second][p] == WALL_CHAR) {
+                    break;
+                }
+            }
+        } else {
+            for(auto p = Robot.first - 1; p >= Robot.first + direction.first - crateCount && p >= 0; p--) {
+                crateCount += (map[Robot.second][p] == CRATE_CHAR);
+                ahead.push_back(map[Robot.second][p]);
+                if(map[Robot.second][p] == WALL_CHAR) {
+                    break;
+                }
+            }
         }
     }
     // Get the first WALL_CHAR in ahead
@@ -91,7 +127,7 @@ void MoveRobot(std::vector<std::vector<char>> & map, std::pair<int,int> & Robot,
         return;
     }
     // Count the number of CRATE_CHAR in ahead
-    int crateCount = std::count(ahead.begin(), ahead.end(), CRATE_CHAR);
+    // int crateCount = std::count(ahead.begin(), ahead.end(), CRATE_CHAR);
     if(crateCount > ahead.size()) {
         // No moves are possible
         direction.first = 0;
@@ -100,53 +136,79 @@ void MoveRobot(std::vector<std::vector<char>> & map, std::pair<int,int> & Robot,
     }
 
     // Push all crates ahead to the edge...
-    for(auto p = 0; p < ahead.size(); p++) {
-        if(direction.first == 0) {
-            if(direction.second > 0) {
-                if(p < ahead.size() - crateCount) {
-                    map[Robot.second + p + 1][Robot.first] = EMPTY_CHAR;
+    if(direction.first == 0) {
+        // Vertical
+        int inc = direction.second > 0 ? 1 : -1;
+        if(inc > 0) {
+            for(auto p = 0; p < ahead.size(); p++) {
+                if(crateCount && p < crateCount) {
+                    map[Robot.second + (ahead.size() - p)][Robot.first] = CRATE_CHAR;
                 } else {
-                    map[Robot.second + p + 1][Robot.first] = CRATE_CHAR;
-                }
-            } else {
-                if(p < ahead.size() - crateCount) {
-                    map[Robot.second - p - 1][Robot.first] = EMPTY_CHAR;
-                } else {
-                    map[Robot.second - p - 1][Robot.first] = CRATE_CHAR;
+                    map[Robot.second + (ahead.size() - p)][Robot.first] = EMPTY_CHAR;
                 }
             }
         } else {
-            if(direction.first > 0) {
-                if(p < ahead.size() - crateCount) {
-                    map[Robot.second][Robot.first + p + 1] = EMPTY_CHAR;
+            for(auto p = 0; p < ahead.size(); p++) {
+                if(crateCount && p < crateCount) {
+                    map[Robot.second - (ahead.size() - p)][Robot.first] = CRATE_CHAR;
                 } else {
-                    map[Robot.second][Robot.first + p + 1] = CRATE_CHAR;
-                }
-            } else {
-                if(p < ahead.size() - crateCount) {
-                    map[Robot.second][Robot.first - p - 1] = EMPTY_CHAR;
-                } else {
-                    map[Robot.second][Robot.first - p - 1] = CRATE_CHAR;
+                    map[Robot.second - (ahead.size() - p)][Robot.first] = EMPTY_CHAR;
                 }
             }
-        }    
+        }
+    } else {
+        // Horizontal
+        int inc = direction.first > 0 ? 1 : -1;
+        if(inc > 0) {
+            for(auto p = 0; p < ahead.size(); p++) {
+                if(crateCount && p < crateCount) {
+                    map[Robot.second][Robot.first + (ahead.size() - p)] = CRATE_CHAR;
+                } else {
+                    map[Robot.second][Robot.first + (ahead.size() - p)] = EMPTY_CHAR;
+                }
+            }
+        } else {
+            for(auto p = 0; p < ahead.size(); p++) {
+                if(crateCount && p < crateCount) {
+                    map[Robot.second][Robot.first - (ahead.size() - p)] = CRATE_CHAR;
+                } else {
+                    map[Robot.second][Robot.first - (ahead.size() - p)] = EMPTY_CHAR;
+                }
+            }
+        }
     }
+
     // ... then move the robot of ahead.size() - crateCount in the right direction
+    // ... and keep a trace of the actual moved performed by the robot, replacing the direction vector
+    // 1. Clear the current position of the robot
     map[Robot.second][Robot.first] = EMPTY_CHAR;
+    // 2. Move the robot
     if(direction.first == 0) {
         if(direction.second > 0) {
+            direction.second = (ahead.size() - crateCount);
             Robot.second += (ahead.size() - crateCount);
         } else {
+            direction.second = -(ahead.size() - crateCount);
             Robot.second -= (ahead.size() - crateCount);
         }
     } else {
         if(direction.first > 0) {
+            direction.first = (ahead.size() - crateCount);
             Robot.first += (ahead.size() - crateCount);
         } else {
+            direction.first = -(ahead.size() - crateCount);
             Robot.first -= (ahead.size() - crateCount);
         }
     }
+    // 3. Set the new position of the robot
     map[Robot.second][Robot.first] = ROBOT_CHAR;
+
+    // Print the result
+    // printCharMap(map);
+    // std::cout << "AFTER: Robot:" << Robot << std::endl;
+    // std::cout << "AFTER: Actual move:" << direction << std::endl;
+    // std::cout << std::endl;
+
 }
 
 int main(int argc, char ** argv, char ** envp) {
@@ -191,16 +253,16 @@ int main(int argc, char ** argv, char ** envp) {
         }
     }
 
-    // Print the map
-    printCharMap(map);
+    // Print the initial state
+    // std::cout << "Moves:" << moves << std::endl;
+    // std::cout << "Robot:" << Robot << std::endl;
     // std::cout << "Map:  " << std::endl << map << std::endl;
-    std::cout << "Moves:" << moves << std::endl;
-    std::cout << "Robot:" << Robot << std::endl;
-
-    // Move the robot around
+    
+    // Read move instructions and Move the robot around
     char lastDirection = 0;
     int distance = 1;
     for(auto p = 0; p < moves.size(); p++) {
+        // Collect move instruction
         char direction = moves[p];
         if(lastDirection == 0) {
             lastDirection = direction;
@@ -210,23 +272,41 @@ int main(int argc, char ** argv, char ** envp) {
             distance++;
             continue;
         }
+
+        // Move the Robot ahead as much as possible
         std::pair<int,int> v = std::make_pair(
               distance * (lastDirection == '<' ? -1 : (lastDirection == '>' ? 1 : 0))
             , distance * (lastDirection == '^' ? -1 : (lastDirection == 'v' ? 1 : 0))
         );
-        
         MoveRobot(map, Robot, v);
-
-        // Print the map
-        std::cout << "Moves:" << moves << std::endl;
-        std::cout << "Robot:" << Robot << std::endl;
-        printCharMap(map);
-        std::cout << std::endl;
 
         // Prepare next iteration
         distance = 1;
         lastDirection = direction;
     }
+    // Do the last move
+    // Move the Robot ahead as much as possible
+    std::pair<int,int> v = std::make_pair(
+          distance * (lastDirection == '<' ? -1 : (lastDirection == '>' ? 1 : 0))
+        , distance * (lastDirection == '^' ? -1 : (lastDirection == 'v' ? 1 : 0))
+    );
+    MoveRobot(map, Robot, v);
+    
+    // Print the result
+    printCharMap(map);
+
+    // Calculate the Crate's Goods Positioning System coordinates
+    // std::set<std::pair<int,int>> GPS;
+    long long SumGPS = 0;
+    for(auto p = 0; p < map.size(); p++) {
+        for(auto q = 0; q < map[p].size(); q++) {
+            if(map[p][q] == CRATE_CHAR) {
+                SumGPS += (100 * p) + (1 * q);
+                // GPS.emplace(q,p);
+            }
+        }
+    }
+    std::cout << "SumGPS: " << SumGPS << std::endl;
 
     return 0;
 }
