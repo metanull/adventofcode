d8e83d5bbe158df67e5d6afce2862fe2bce59794
PascalHavelange <havelangep@gmail.com>
2025-01-28 15:02:41 +0100

    Adding room for 2024 day20

---
+++ 2024/Day20/Constant.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANT_H
+#define CONSTANT_H
+
+// #define UPPERLIMIT  LONG_MAX
+#define DEBUG       0
+#define MULTITHREAD 1
+
+// #define INPUT_PATH  "./input.txt"
+#define INPUT_PATH  "E:/adventofcode/2024/Input/Day20.txt"
+// #define INPUT_PATH "C:/Users/phave/Documents/Development/adventofcode/2024/Input/Day20.txt"
+
+#endif
---
+++ 2024/Day20/Day20.1.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <stack>
+#include <algorithm>
+#include <filesystem>
+#include <thread>
+#include <chrono>
+#include <set>
+
+#include "Constant.h"
+
+#include "charmap.h"
+#include "math.h"
+
+// ---------------------------------------------------------
+const char *banner = "AdventOfCode 2024 Day 20!";
+const char *inputFilePath = INPUT_PATH;
+
+int main(int argc, char **argv, char **envp)
+{
+
+    std::vector<std::string> args(argv, argv + argc);
+    if (args.size() == 1)
+    {
+        // No input file provided, use the default one
+        args.push_back(inputFilePath);
+    }
+    size_t banner_width = 80;
+    std::cout << "\033[7;34;1m" << std::endl
+              << std::setfill('#') << std::setw(banner_width) << '#' << std::endl
+              << std::setw((banner_width - strlen(banner)) / 2) << '#'
+              << banner
+              << std::setw(((banner_width - strlen(banner)) / 2) + ((banner_width - strlen(banner)) % 2)) << '#' << std::endl
+              << std::setfill('#') << std::setw(banner_width) << '#' << std::endl
+              << "\033[0m" << std::endl
+              << std::endl;
+
+    metanull::charmap::map inputMap;
+    // Read the input file, and initialize the maze
+    // Read input file
+    std::ifstream inputFile(args[1]);
+    if (!inputFile || !inputFile.is_open())
+    {
+        std::cerr << "Unable to open " << inputFilePath << "." << std::endl;
+        std::cerr << "Current working directory: " << std::filesystem::current_path() << std::endl;
+        return 1;
+    }
+    inputFile >> inputMap;
+    inputFile.close();
+
+    std::cout << "Input map:" << inputMap << std::endl;
+    auto start = metanull::charmap::char_find(inputMap, 'S').front();
+    auto end = metanull::charmap::char_find(inputMap, 'E').front();
+    std::cout << "Start: " << start.first << "," << start.second << std::endl;
+    std::cout << "End: " << end.first << "," << end.second << std::endl;
+
+    auto b = metanull::charmap::maze_find_first_best_path(inputMap, start, end, metanull::charmap::EAST, metanull::charmap::basic_score_move, metanull::charmap::is_tile_free);
+    if(b.empty()) {
+        std::cerr << "No path found" << std::endl;
+        return 1;
+    }
+    metanull::charmap::maze_print_path(std::cout, inputMap, start, end, b, metanull::charmap::is_tile_free);
+    /*
+    for(auto p : b.front().visited) {
+        std::vector<metanull::charmap::position> s;
+        s.push_back(metanull::charmap::translate(metanull::charmap::translate(p,metanull::charmap::NORTH),metanull::charmap::NORTH)); 
+        s.push_back(metanull::charmap::translate(metanull::charmap::translate(p,metanull::charmap::EAST),metanull::charmap::EAST)); 
+        s.push_back(metanull::charmap::translate(metanull::charmap::translate(p,metanull::charmap::SOUTH),metanull::charmap::SOUTH)); 
+        s.push_back(metanull::charmap::translate(metanull::charmap::translate(p,metanull::charmap::WEST),metanull::charmap::WEST)); 
+        for(auto c : s) {
+            auto f = std::find(b.front().visited.begin() + std::distance(), b.front().visited.end(), c);
+            distance b, f = gain shortcut
+        }
+    }
+    */
+    return 0;
+}
