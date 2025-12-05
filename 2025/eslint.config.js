00572e777ce48ab0239050edcb7b8baff3c8cc1f
PascalHavelange <havelangep@gmail.com>
2025-12-05 14:16:49 +0100

    2025 Day1 (#2)

---
+++ 2025/eslint.config.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        performance: 'readonly'
+      }
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single', { avoidEscape: true }]
+    }
+  }
+];
